8940c8d1e178b7e070c7c25a1d8e29a12ce1b9bb
zc <zchen@cse.msstate.edu>
2022-11-14 21:37:26 -0600

    update

--- models.py
+++ models.py
@@ -127,8 +127,8 @@ class SGC(torch.nn.Module):
 class ARMA(torch.nn.Module):
     def __init__(self, in_channels, hidden_channels, out_channels):
         super().__init__()
-        self.conv1 = ARMAConv(in_channels, hidden_channels)
-        self.conv2 = ARMAConv(hidden_channels, out_channels)
+        self.conv1 = ARMAConv(in_channels, out_channels)
+        # self.conv2 = ARMAConv(hidden_channels, out_channels)
 
     def forward(self, x, edge_index, edge_weight=None):
         x = F.dropout(x, p=0.5, training=self.training)
@@ -194,4 +194,4 @@ class GCN2(torch.nn.Module):
 # Linear: GCN, Sage, GIN, GAT?
 # Poly: ChebNet, SGC, HCG?, GCN2?
 # Rat: ARMA, GAPP
-__all__ = ['GCN', 'Sage', 'GIN', 'GAT', 'ChebNet', 'SGC', 'HCG', 'GCN2', 'GAPP', 'ARMA']
+__all__ = ['GCN', 'Sage', 'GIN', 'ChebNet', 'SGC', 'GAPP', 'ARMA']
