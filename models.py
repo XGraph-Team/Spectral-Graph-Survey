606bc84468d5ea53162755ddebebb12e0e8bbe90
aquastar <aquastar@users.noreply.github.com>
2022-11-15 15:35:22 -0600

    update

--- models.py
+++ models.py
@@ -22,8 +22,8 @@ class GCN(torch.nn.Module):
 class Sage(torch.nn.Module):
     def __init__(self, in_channels, hidden_channels, out_channels):
         super().__init__()
-        self.conv1 = SAGEConv(in_channels, hidden_channels)
-        self.conv2 = SAGEConv(hidden_channels, out_channels)
+        self.conv1 = SAGEConv(in_channels, hidden_channels, normalize=True)
+        self.conv2 = SAGEConv(hidden_channels, out_channels, normalize=True)
 
     def forward(self, x, edge_index, edge_weight=None):
         x = F.dropout(x, p=0.5, training=self.training)
